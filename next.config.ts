1e79c6ca340fa233db9226b98a658a2f4192c445
razibhossain-hbk <ext_razib.hossain@hbkworld.com>
2025-11-24 15:29:05 +0600

    github actions workflow added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Enable static export for GitHub Pages
+  output: "export",
+  // Disable image optimization to allow static export
+  images: {
+    unoptimized: true,
+  },
+  // If this is a project site like https://<user>.github.io/devops
+  // then the repository name should be used as basePath.
+  basePath: "/devops",
+  assetPrefix: "/devops/",
 };
 
 export default nextConfig;
